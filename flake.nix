212dacec66264ae8acc566bb1ce5b3c78ac253f4
b0xfox <cwashin333@gmail.com>
2024-08-02 16:22:13 -0400

    Fixed errors in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,28 +1,27 @@
 {
-	description = "My NixOS Flake"'
+	description = "My NixOS Flake";
 
 	inputs = {
 		nixpkgs.url = "nixpkgs/nixos-24.05";
-		home-manager.url = "github:nix-community/home-manager/release-24.05";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+        home-manager = {
+            url = "github:nix-community/home-manager/release-24.05";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 	};
 
 	outputs = { self, nixpkgs, ... }:
     let
-        lib = nixpkgs.lib;
         system = "x86_64-linux";
-        hostName = "box";
-        userName = "fox";
         pkgs = nixpkgs.legacyPackages.${system};
     in {
-        nixosConfigurations = {
-            ${hostName} = lib.nixosSystem {
-                system = ${system};
-                modules = [ ./system-config/configuration.nix ];
-            };
+        # box - system hostname
+        nixosConfigurations.box = nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [ ./system-config/configuration.nix ];
         };
         homeConfigurations = {
-            ${username} = home-manager.lib.homeManagerConfiguration {
+            fox = home-manager.lib.homeManagerConfiguration {
                 inherit pkgs;
                 modules = [ ./system-config/home.nix ];
             };
